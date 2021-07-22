51c96fe242d050a91b2243cb5f20edc7a42a61fd
Zachary <73163517+Serade12@users.noreply.github.com>
2021-07-22 13:36:56 +0800

    Update init_mysql.sql

--- init_mysql.sql
+++ init_mysql.sql
@@ -5,3 +5,5 @@ grant all privileges on cost.* TO 'cost'@'localhost';
 use cost;
 drop table if exists sensors;
 create table sensors(ID int not null auto_increment,Appliance_Name varchar(100),Status tinyint(1) not null,Start_Time timestamp,primary key(id));
+insert into `sensors` (`ID`,`Appliance_Name`,`Status`,`Start_Time`) VALUES (1,'Aircon','0', current_timestamp()), (2,'TV',1,current_timestamp());
+
