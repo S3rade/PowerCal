a3877bc5c755c486f0213fb5a76e0ff322c1a584
Zachary <73163517+Serade12@users.noreply.github.com>
2021-07-23 22:56:16 +0800

    Update iot_sensor.py

--- iot_sensor.py
+++ iot_sensor.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
 
-# Code Cell 1.
+#Import MQTT connector module.
+import paho.mqtt.client as mqtt 
 
-# Import the dweepy module that is a collection of functions that make it  
-# easier to communicate with dweet.io 
+# Import the dweepy module that is a collection of functions that make it  easier to communicate with dweet.io 
 import dweepy
 
-#===================
 myThing = "0353B_sensor" #Add value: add your dweet thing name
-#===================
 
-# Import the GPIO modules to control the GPIO pins of the Raspberry Pi
-# Uncomment the following only when testing on a physcial Rasberry Pi
-# Comment the following when testing on a Raspbian VM
+# Import the GPIO modules to control the GPIO pins of the Raspberry Pi, Uncomment the following only when testing on a physcial Rasberry Pi
+#Comment the following when testing on a Raspbian VM
 #import RPi.GPIO as GPIO
 
-# Import the Mock GPIO modules to control the Mock GPIO pins of the Raspberry Pi
-# Uncomment the following when testing on a Raspbian VM
+# Import the Mock GPIO modules to control the Mock GPIO pins of the Raspberry Pi,Uncomment the following when testing on a Raspbian VM
 # Comment the following when testing on a physcial Rasberry Pi
-from GPIOEmulator.EmulatorGUI import GPIO
+import EmulateGPIO as GPIO
 
 # Import to clear cell output with code
 from IPython.display import clear_output
@@ -27,33 +23,45 @@ from IPython.display import clear_output
 import time
 
 import os
-
 _=os.system("clear")
 
-# Code Cell 2.
-#Setup hardware
-# Set the desired pin numbering scheme:
+#Setup hardware, Set the desired pin numbering scheme:
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 #Create variables for the GPIO PINs the LEDs are connected to
-# ============================================
 # the PIN of the green LED
 GreenLEDPin = 20   #Add values: add the pin number for the green LED
 # the PIN of the red LED
 RedLEDPin   = 21   #Add values: add the pin number for the red LED
-#=============================================
 
-# Setup the direction of the GPIO pins - either INput or OUTput 
-# The PINs that connect LEDs must be set to OUTput mode:
+# Setup the direction of the GPIO pins - either INput or OUTput, The PINs that connect LEDs must be set to OUTput mode:
 GPIO.setup(GreenLEDPin, GPIO.OUT)
 GPIO.setup(RedLEDPin, GPIO.OUT)
 GPIO.output(GreenLEDPin, False) # True = set 3.3V on the pin
 GPIO.output(RedLEDPin,   False) #False = set 0V on the pin
 print('All LEDs are turned OFF' + '\n')
 
-#Code Cell 4.
+#MQTT Details
+broker="172.16.0.12"
+port = 1883
+client = mqtt.Client("IOT-Sensor-Publisher")
+client.username_pw_set(username= "root",password="kali")
+
+
+#MQTT Functions
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code", rc)
+
+def on_publish(client, userdata,result):
+  	print ("Data is being Published \n")
+
+def on_disconnect(client, userdata, rc):
+    print("Broker has Disconnected!")
+
+
 while True:
+	
 	# Asks the user to select the LED. Put the response into a variable.
 	lit = input("Is the Device on or off ? (o)n to ON or (f) to OFF ? (q) to quit: ")
     
@@ -61,21 +69,34 @@ while True:
 	lit1 = lit.lower()
 
 	#Set the LED state based on the user input
-	if lit1 == "f": #If the user chose the red LED
+	if lit1 == "f": #If the user chose the Turn OFF the Device
 		print("Turning Off the Device")
 		GPIO.output(GreenLEDPin, False) # False = set 0V on the pin
 		GPIO.output(RedLEDPin,   True)  # True = set 3.3V on the pin
 		print('sending dweet...')
+
+		#MQTT codes		
+		client.connect(broker) #Connects to broker
+		client.loop_start()
+		print('Publishing ... ') 
+		client.publish("iot/Aircon","ON")		
+		print('Published !')
+		time.sleep(4)
+		client.loop_stop()
+		
+
 		old_dweet = dweepy.dweet_for(myThing, {"dweet": "Red"}) #this function sends data to dweet.io
 		old_created = old_dweet['created'] #get the time stamp of the first dweet
+		
 		print('send dweet@' , old_created + '\n')
 		break
 
-	elif  lit1 == "o": #If the user chose the green LED
+	elif  lit1 == "o": #If the user chose the Turn ON the Device
 		print("Turning On the Device")
 		GPIO.output(GreenLEDPin, True) # True = set 3.3V on the pin
 		GPIO.output(RedLEDPin,   False) #False = set 0V on the pin
 		print('sending dweet...')
+				
 		old_dweet = dweepy.dweet_for(myThing, {"dweet": "Green"}) #this function sends data to dweet.io
 		old_created = old_dweet['created'] #get the time stamp of the first dweet
 		print('send dweet@' , old_created + '\n')
@@ -84,13 +105,13 @@ while True:
 	elif  lit1 == "q": #If the user chose to quit the program
 		GPIO.output(GreenLEDPin, False) # True = set 3.3V on the pin
 		GPIO.output(RedLEDPin,   False) #False = set 0V on the pin
-		print('Deavtivate All LEDs' + '\n')
+		print('Quiting Programme' + '\n')
 		exit()
 
-	else:  #If the user entered something other than r, g, or q.
+	else:  #If the user entered something other than o, f, or q.
 		print("Please enter (o)n to ON or (f) to OFF ? (q) to quit:")
 
-#Code cell 6.
+
 
 counter = 0
 
@@ -107,6 +128,16 @@ while True:
 			print("Turning Off the Device")
 			GPIO.output(GreenLEDPin, False) # False = set 0V on the pin
 			GPIO.output(RedLEDPin,   True)  # True = set 3.3V on the pin
+			
+			#MQTT codes		
+			client.connect(broker) #Connects to broker
+			client.loop_start()
+			print('Publishing ... ') 
+			client.publish("iot/Aircon","OFF")		
+			print('Published !')
+			time.sleep(4)
+			client.loop_stop()
+			
 			lit1 = "f"
 			print()
 
@@ -115,6 +146,15 @@ while True:
 			print("Turning On the Device")
 			GPIO.output(GreenLEDPin, True) 
 			GPIO.output(RedLEDPin,   False)
+			
+			#MQTT codes		
+			client.connect(broker) #Connects to broker
+			client.loop_start()
+			print('Publishing ... ') 
+			client.publish("iot/Aircon","ON")		
+			print('Published !')
+			time.sleep(4)
+			client.loop_stop()
 			lit1 = "o"
 			print()
 	time.sleep(1)
