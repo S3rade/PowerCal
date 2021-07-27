2b0eb0b5ee0f0312081b56b20247ff3dbe3d7101
Zachary <73163517+Serade12@users.noreply.github.com>
2021-07-27 17:17:21 +0800

    Delete init_mysql.sql

--- init_mysql.sql
+++
@@ -1,8 +0,0 @@
-drop user if exists 'cost'@'localhost';
-create user 'cost'@'localhost' IDENTIFIED BY 'CostGroupA';
-create database if not exists cost;
-grant all privileges on cost.* TO 'cost'@'localhost';
-use cost;
-drop table if exists sensors;
-create table sensors(ID int not null auto_increment,Appliance_Name varchar(100),Status tinyint(1) not null,Start_Time timestamp,primary key(id));
-insert into `sensors` (`ID`,`Appliance_Name`,`Status`,`Start_Time`) VALUES (1,'Aircon','0', current_timestamp()), (2,'Lights','0',current_timestamp()), (3,'Fan','0',current_timestamp());
