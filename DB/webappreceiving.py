ad14a1bf54ce02cd8eee69558a8cf1c3ff3f78f4
J3rick <64952343+J3rick@users.noreply.github.com>
2021-07-31 14:01:16 +0800

    Update webappreceiving.py

--- DB/webappreceiving.py
+++ DB/webappreceiving.py
@@ -5,66 +5,72 @@ import time
 import paho.mqtt.client as mqtt
 import re
 import os
-_=os.system("clear" 
+_=os.system("clear")
 
 #MQTT Details
-broker ="172.16.0.12"
+broker="172.16.0.12"
 port = 1883
-client = mqtt.Client("IOT-WebDB-Subscriber")
+client = mqtt.Client("IOT-DB-Subscriber")
 client.username_pw_set(username= "root",password="kali")
+certfilepath= "/root/iot_vol/scripts/cacert/ca.crt"
+client.tls_set(certfilepath,tls_version=2)
+client.tls_insecure_set(False)
 
 #MQTT Functions
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code", rc)
+    print("Connected with result code".format(str(rc)))
+    client.subscribe("iot")
 
-def on_publish(client, userdata,result):
-  	print ("Data is being Published \n")
-
-def on_disconnect(client, userdata, rc):
-    print("Broker has Disconnected!")
-
-while True:
-    
-def on_message(client, userdata, message):
-    Messagereceived =True
-
-    appliance_msg = str(message.payload.decode("utf-8"))
-    topic = str(message.topics)
-
+def on_message(client, userdata, msg):
+    Messagereceived = True
+    apayload = str(msg.payload)
+    newpayload = apayload[2:-1]
+    appliance_msg = newpayload
+    print (appliance_msg)
     #filter by appliance name and status
-    appliance_msg = appliance_msg.split(",")
+    split_msg = appliance_msg.split(":")
     split_msg_sorted = sorted(split_msg)
     print(split_msg_sorted)
-
-    appliance_name = split_msg_sorted[1].split(":")[1]
-    appliance_status = split_msg_sorted[2].split(":")[1]
-
+    appliance_name = split_msg_sorted[0].split(":")
+    appliance_status = split_msg_sorted[1].split(":")
     appliance_list = ['Aircon', 'Lights']
+    print (appliance_list)
     #If appliance name inside
+    appliance_name = str(appliance_name)
+    appliance_name = appliance_name[2:-2]
+    print (appliance_name)
     if appliance_name in appliance_list:
         position = appliance_list.index(appliance_name)
-        print(position) 
+        appliance_status = str(appliance_status)
+        appliance_status = appliance_status[2:-2]
+        print (appliance_status) 
         if (position == 0):
             device_id = "1"
-            deviceStatus = "1"
+            if appliance_status == "OFF":
+                deviceStatus = "0"
+                os.system('curl http://172.16.0.13:8080/db/update/'+device_id+'?"Status='+deviceStatus+'"')
+                Messagereceived=False
+            else:
+                deviceStatus = "1"
+                os.system('curl http://172.16.0.13:8080/db/update/'+device_id+'?"Status='+deviceStatus+'"')
+                Messagereceived=False
         elif (position == 1):
             device_id = "2"
-            device_status= "1"
-    os.system('curl http://172.16.0.13:8080/update/'+device_id+'?"DeviceStatus='+deviceStatus+'"')
+            deviceStatus= "1"
+            os.system('curl http://172.16.0.13:8080/db/update/'+device_id+'?"Status='+deviceStatus+'"')
+            Messagereceived=False
     else:
         print("Appliance name not recognised")
         print ("Adding Appliance into the Database")
-    os.system('curl http://172.16.0.13:8080/add/'+appliance_name+'?"&DeviceStatus='+deviceStatus+'"')
-
-Messagereceived=False
+        os.system('curl http://172.16.0.13:8080/add/'+appliance_name+'?"Status='+deviceStatus+'"')
+        Messagereceived=False
 
 connected=False
 
-client.username_pw_set(username="",password="")
-client.on_message = on_message
-client.connect(broker)
+client.username_pw_set(username="root",password="kali")
 client.on_connect = on_connect
-client.subscribe("farm")
+client.connect(broker)
+client.on_message = on_message
 client.loop_start()
 while connected != True:
     time.sleep(0.1)
