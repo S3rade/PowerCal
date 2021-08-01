f927ee8fda04a25f72f3afd4f278076d1f572164
Augmen So <73163530+Augmenso@users.noreply.github.com>
2021-08-01 20:14:14 +0800

    Firewall up

--- DB/start_webdb.sh
+++ DB/start_webdb.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+#if not working, remember to " modprobe ip6table_filter " on kali first
+ufw allow ssh
+ufw allow 8080/tcp
+ufw allow 80/tcp
+ufw enable
+ufw limit 8080/tcp
 cd /root/iot_vol/scripts
 service mysql start
 mysql < init_mysql.sql
